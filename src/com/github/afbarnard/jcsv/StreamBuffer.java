befba2ae90bd65057025c08f6a9cbe4255e9933d
Aubrey Barnard <aubrey.f.barnard@gmail.com>
2013-11-07 20:31:02 -0600

    Created dynamic buffer for use with processing streams.
    * Created accompanying test.

---
+++ src/com/github/afbarnard/jcsv/StreamBuffer.java
@@ -0,0 +1,185 @@
+package com.github.afbarnard.jcsv;
+
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+
+/**
+ * A sliding view of a sequence (or stream) with random access to the
+ * contents implemented as a dynamic circular buffer.  In other words, a
+ * dynamic queue that indexes its elements and provides random access to
+ * its contents.
+ */
+public class StreamBuffer<E> {
+
+    /** Array that stores the contents of the buffer. */
+    private Object[] buffer;
+
+    /**
+     * The buffer index that corresponds to the lower sequence index.
+     */
+    private int lowerIndex = 0;
+
+    /**
+     * The sequence index of the head of the queue, the lower bound on
+     * the buffer content.
+     */
+    private long lower = 0;
+
+    /**
+     * The sequence index of the first available spot in the buffer, the
+     * tail (plus one) of the queue, the upper bound on the buffer
+     * content (in the sense that the content occupies indices
+     * [lower,upper)).
+     */
+    private long upper = 0;
+
+    public boolean debug = false;
+
+    public StreamBuffer() {
+        this(1000);
+    }
+
+    public StreamBuffer(int initialCapacity) {
+        buffer = new Object[initialCapacity];
+        // All the numbers start at zero
+    }
+
+    public int capacity() {
+        return buffer.length;
+    }
+
+    public int size() {
+        return (int)(upper - lower);
+    }
+
+    public long lower() {
+        return lower;
+    }
+
+    public long upper() {
+        return upper;
+    }
+
+    public void put(E element) {
+        debug("put()", upper);
+        // Make sure there is room
+        if ((int)(upper - lower) >= buffer.length) {
+            growBuffer(buffer.length + 1);
+        }
+
+        // Add the element
+        buffer[bufferIndex(upper)] = element;
+        upper++;
+    }
+
+    /**
+     * Gets the element with the lowest sequence index and removes it
+     * from the buffer (frees its location).  Equivalent to
+     * <code>getAt(lower())</code> followed by
+     * <code>free(lower())</code>.
+     */
+    public E get() {
+        debug("get()", lower);
+        if (lower >= upper)
+            throw new NoSuchElementException();
+        E element = buffer(bufferIndex(lower));
+        internalFree(lower);
+        return element;
+    }
+
+    public E getAt(long position) {
+        debug("getAt()", position);
+        checkPosition(position);
+        return buffer(bufferIndex(position));
+    }
+
+    public void free(long position) {
+        debug("free()", position);
+        // Only free valid positions, ignore any previously freed
+        // positions
+        if (position >= lower) {
+            checkPosition(position);
+            internalFree(position);
+        }
+    }
+
+    ////////////////////////////////////////
+    // Internal
+
+    private void checkPosition(long position) {
+        if (position < lower || position >= upper) {
+            throw new NoSuchElementException(
+                String.format("Position %d is not in [%d,%d)",
+                              position, lower, upper)
+            );
+        }
+    }
+
+    private int bufferIndex(long position) {
+        return (lowerIndex + (int)(position - lower)) % buffer.length;
+    }
+
+    // Do type<E> access to the array the same way as in ArrayList
+    @SuppressWarnings("unchecked")
+    private E buffer(int index) {
+        return (E) buffer[index];
+    }
+
+    private void internalFree(long position) {
+        // How much to free? (includes 'position')
+        int amount = (int)(position - lower) + 1;
+        // Move up lower buffer index limit (with wrap-around)
+        lowerIndex = (lowerIndex + amount) % buffer.length;
+        // Move up lower sequence index limit
+        lower = position + 1;
+    }
+
+    private void growBuffer(int minimumCapacity) {
+        // Grow the buffer by at least a power of two
+        int capacity = Math.max(buffer.length * 2, minimumCapacity);
+
+        // Allocate a new buffer
+        Object[] newBuffer = new Object[capacity];
+
+        // Copy the contents of the old buffer to the start of the new
+        // buffer discarding any circularity.  Only copy contents if the
+        // buffer is not empty.  If lowerIndex == upperIndex then the
+        // buffer is either full or empty, so use the positions to
+        // discriminate.
+        int upperIndex = bufferIndex(upper);
+        if (lowerIndex < upperIndex) {
+            // The buffer has content that is linear.  Do a single
+            // linear copy.
+            System.arraycopy(buffer, lowerIndex, newBuffer, 0,
+                             (upperIndex - lowerIndex));
+        } else if (lowerIndex > upperIndex || lower < upper) {
+            // The buffer has content that is wrapped.  Do two linear
+            // copies for the two chunks.
+            int chunkSize = buffer.length - lowerIndex;
+            System.arraycopy(buffer, lowerIndex, newBuffer, 0,
+                             chunkSize);
+            // Protect against empty copies
+            if (upperIndex > 0) {
+                System.arraycopy(buffer, 0, newBuffer, chunkSize,
+                                 upperIndex);
+            }
+        }
+        // Else the buffer is empty and no copy is needed
+
+        // Use the new buffer
+        buffer = newBuffer;
+
+        // Reset the buffer offset of 'lower'
+        lowerIndex = 0;
+    }
+
+    private void debug(String source, long position) {
+        if (debug) {
+            System.out.println(String.format("%s: @%d: %s", source, position, toDebugString()));
+        }
+    }
+
+    private String toDebugString() {
+        return String.format("[%d,%d) -> [%d,%d); %s", lower, upper, lowerIndex, bufferIndex(upper), Arrays.toString(buffer));
+    }
+}
---
+++ src/com/github/afbarnard/jcsv/StreamBufferTest.java
@@ -0,0 +1,324 @@
+package com.github.afbarnard.jcsv;
+
+import java.util.NoSuchElementException;
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class StreamBufferTest {
+
+    static final int BUFFER_SIZE = 10;
+
+    static final Integer[] sequence = {
+        58, 76, 21, 29,  6, 88, 65, 55, 73, 92,
+        15, 33, 77, 24,  5,  9, 12, 49, 46, 50,
+         4, 45, 36, 11, 70, 40, 39, 47, 48, 61,
+        13, 66, 67, 52, 81, 72, 10, 94, 60, 44,
+        56, 20, 17, 87, 63, 25, 97, 74, 80, 69,
+        31, 71, 95, 62, 41, 53, 30, 78, 82, 16,
+        83, 93, 14, 91, 90, 51, 26, 32, 22,  2,
+        23, 57,  3, 42, 27,  8, 43, 79, 96, 99,
+        68, 54,  7, 37, 34, 75, 59, 89, 64,  1,
+        28, 18, 85, 19, 35, 38, 98, 86,  0, 84
+    };
+
+    StreamBuffer<Integer> buffer;
+
+    // Helper to load content into buffer
+    private void putIntoBuffer(int amount) {
+        for (int i = 0; i < amount; i++) {
+            buffer.put(sequence[i]);
+        }
+    }
+
+    private void checkSizeCapacity(int size, int capacity) {
+        assertEquals(size, buffer.size());
+        assertEquals(capacity, buffer.capacity());
+    }
+
+    private void checkLowerUpper(long lower, long upper) {
+        assertEquals(lower, buffer.lower());
+        assertEquals(upper, buffer.upper());
+    }
+
+    @Before public void setUp() {
+        buffer = new StreamBuffer<Integer>(BUFFER_SIZE);
+    }
+
+    ////////////////////////////////////////
+    // Basics
+
+    @Test public void newSizeCapacity() {
+        checkSizeCapacity(0, BUFFER_SIZE);
+    }
+
+    @Test public void newPositions() {
+        checkLowerUpper(0, 0);
+    }
+
+    @Test(expected=NoSuchElementException.class)
+    public void noSuchElement() {
+        buffer.get();
+    }
+
+    @Test public void putGet() {
+        Integer i = Integer.valueOf(647);
+        buffer.put(i);
+        assertEquals(i, buffer.get());
+        checkSizeCapacity(0, BUFFER_SIZE);
+        checkLowerUpper(1, 1);
+    }
+
+    @Test public void putGetAt() {
+        Integer i = Integer.valueOf(128);
+        buffer.put(i);
+        assertEquals(i, buffer.getAt(0));
+        checkSizeCapacity(1, BUFFER_SIZE);
+        checkLowerUpper(0, 1);
+    }
+
+    @Test public void putFree() {
+        Integer i = Integer.valueOf(263);
+        buffer.put(i);
+        buffer.free(0);
+        checkSizeCapacity(0, BUFFER_SIZE);
+        checkLowerUpper(1, 1);
+    }
+
+    @Test public void freeHalf() {
+        int full = BUFFER_SIZE;
+        int half = full / 2;
+        Object[][] pattern = {{'w', full}, {'f', half}};
+        checkAccess(sequence, pattern, BUFFER_SIZE);
+        checkSizeCapacity(half, BUFFER_SIZE);
+        checkLowerUpper(half, full);
+    }
+
+    @Test public void freeAll() {
+        int amount = 5;
+        Object[][] pattern = {{'w', amount}, {'f', amount}};
+        checkAccess(sequence, pattern, BUFFER_SIZE);
+        checkSizeCapacity(0, BUFFER_SIZE);
+        checkLowerUpper(amount, amount);
+    }
+
+    @Test public void growCapacityAfterGet() {
+        // Insert enough to cause a resize after writing and reading
+        // initial content
+        int amount = BUFFER_SIZE * 2;
+        Object[][] pattern = {{'w', 1}, {'r', 1}, {'w', amount}};
+        checkAccess(sequence, pattern, BUFFER_SIZE);
+        checkSizeCapacity(amount, BUFFER_SIZE * 2);
+    }
+
+    @Test public void growCapacityAfterFree() {
+        // Insert enough to cause a resize after writing and freeing
+        // initial content
+        int amount = BUFFER_SIZE + 1;
+        Object[][] pattern = {{'w', 1}, {'f', 1}, {'w', amount}};
+        checkAccess(sequence, pattern, BUFFER_SIZE);
+        checkSizeCapacity(amount, BUFFER_SIZE * 2);
+    }
+
+    @Test public void freeOld() {
+        Integer i = Integer.valueOf(410);
+        buffer.put(i);
+        buffer.put(i);
+        buffer.free(0);
+        buffer.free(1);
+        buffer.free(0);
+        buffer.free(1);
+        checkSizeCapacity(0, BUFFER_SIZE);
+        checkLowerUpper(2, 2);
+    }
+
+    ////////////////////////////////////////
+    // Access patterns
+
+    @Test public void checkAccess_oneByOne() {
+        Object[][] pattern = {{'w', 1}, {'r', 1}, {'w', 1}, {'f', 1}};
+        checkAccess(sequence, pattern, BUFFER_SIZE);
+        assertEquals(0, buffer.size());
+    }
+
+    @Test public void checkAccess_oneByOneFull() {
+        // Make sure the buffer goes around at least once
+        Object[][] pattern =
+            {{'w', BUFFER_SIZE}, {'r', 1}, {'w', 1}, {'f', 1},
+             {'w', 1}, {'r', 1}, {'w', 1}, {'f', 1}, {'w', 1}, {'f', 1},
+             {'w', 1}, {'r', 1}, {'w', 1}, {'r', 1}, {'w', 1}, {'f', 1},
+             {'w', 1}, {'r', 1}, {'w', 1}, {'f', 1}, {'w', 1}, {'r', 1},
+             {'w', 1}, {'r', 1}, {'w', 1}, {'r', 1}};
+        checkAccess(sequence, pattern, BUFFER_SIZE);
+        assertEquals(BUFFER_SIZE - 1, buffer.size());
+    }
+
+    @Test public void checkAccess_chunksLessCapacity() {
+        int amount = 3 * BUFFER_SIZE / 4;
+        Object[][] pattern =
+            {{'w', amount}, {'r', amount},
+             {'w', amount}, {'f', amount},
+             {'w', amount}, {'f', amount},
+             {'w', amount}, {'r', amount}};
+        checkAccess(sequence, pattern, BUFFER_SIZE);
+        assertEquals(0, buffer.size());
+    }
+
+    @Test public void checkAccess_chunksCapacity() {
+        int amount = BUFFER_SIZE;
+        Object[][] pattern =
+            {{'w', amount}, {'f', amount},
+             {'w', amount}, {'r', amount},
+             {'w', amount}, {'r', amount},
+             {'w', amount}, {'f', amount}};
+        checkAccess(sequence, pattern, BUFFER_SIZE);
+        assertEquals(0, buffer.size());
+    }
+
+    @Test public void checkAccess_chunksMoreCapacity() {
+        int amount = BUFFER_SIZE + 1;
+        Object[][] pattern =
+            {{'w', amount}, {'r', amount},
+             {'w', amount}, {'f', amount},
+             {'w', amount}, {'r', amount},
+             {'w', amount}, {'r', amount}};
+        checkAccess(sequence, pattern, BUFFER_SIZE);
+        assertEquals(0, buffer.size());
+    }
+
+    @Test public void checkAccess_queueLessCapacity() {
+        int amount = BUFFER_SIZE / 2;
+        Object[][] pattern =
+            {{'w', amount - 1},
+             {'w', amount}, {'r', amount},
+             {'w', amount}, {'r', amount},
+             {'w', amount}, {'f', amount},
+             {'w', amount}, {'r', amount},
+             {'w', amount}, {'f', amount},
+             {'r', amount - 1}};
+        checkAccess(sequence, pattern, BUFFER_SIZE);
+        assertEquals(0, buffer.size());
+    }
+
+    @Test public void checkAccess_queueCapacity() {
+        int amount = BUFFER_SIZE;
+        Object[][] pattern =
+            {{'w', amount},
+             {'w', amount}, {'f', amount},
+             {'w', amount}, {'r', amount},
+             {'w', amount}, {'f', amount},
+             {'w', amount}, {'f', amount},
+             {'r', amount}};
+        checkAccess(sequence, pattern, BUFFER_SIZE);
+        assertEquals(0, buffer.size());
+    }
+
+    @Test public void checkAccess_queueMoreCapacity() {
+        int amount = 3 * BUFFER_SIZE / 2;
+        Object[][] pattern =
+            {{'w', amount},
+             {'w', amount}, {'r', amount},
+             {'w', amount}, {'r', amount},
+             {'r', amount}};
+        checkAccess(sequence, pattern, BUFFER_SIZE);
+        assertEquals(0, buffer.size());
+    }
+
+    @Test public void checkAccess_pattern01() {
+        Object[][] pattern =
+            {{'w',  8}, {'f',  6}, {'w', 12}, {'r', 14}, // 20, 0
+             {'w',  4}, {'w', 13}, {'f', 12}, {'w', 14}, // 51, 19
+             {'w',  6}, {'f',  7}, {'w',  4}, {'f', 14}, // 61, 8
+             {'r',  8}};
+        checkAccess(sequence, pattern, BUFFER_SIZE);
+        assertEquals(0, buffer.size());
+    }
+
+    @Test public void checkAccess_pattern02() {
+        Object[][] pattern =
+            {{'w', 17}, {'r',  6}, {'w', 10}, {'w', 18}, // 45, 39
+             {'w', 13}, {'w', 14}, {'w',  6}, {'f',  3}, // 78, 69
+             {'w', 14}, {'w',  5}, {'f', 17}, {'w',  3}, // 100, 74
+             {'f',  6}, {'r',  7}, {'f',  6}, {'r', 20}, // 100, 35
+             {'r',  9}, {'r', 17}};                      // 100, 9
+        checkAccess(sequence, pattern, BUFFER_SIZE);
+        assertEquals(9, buffer.size());
+    }
+
+    @Test public void checkAccess_pattern03() {
+        Object[][] pattern =
+            {{'w',  4}, {'f',  4}, {'w', 15}, {'f', 12}, // 19, 3
+             {'w',  1}, {'w', 11}, {'w',  3}, {'f',  2}, // 34, 16
+             {'w', 10}, {'w',  7}, {'w',  6}, {'w', 13}, // 70, 52
+             {'r', 13}, {'r', 13}, {'f', 13}};           // 70, 13
+        checkAccess(sequence, pattern, BUFFER_SIZE);
+        assertEquals(13, buffer.size());
+    }
+
+    @Test public void checkAccess_pattern04() {
+        Object[][] pattern =
+            {{'w',  8}, {'w',  9}, {'w', 45}, {'f', 44}, // 62, 18
+             {'w', 17}, {'f', 23}, {'w',  6}, {'r',  7}  // 85, 11
+            };
+        checkAccess(sequence, pattern, BUFFER_SIZE);
+        assertEquals(11, buffer.size());
+    }
+
+    /**
+     * Simulates and checks various access patterns according to the
+     * given schedule.
+     */
+    private void checkAccess(Integer[] sequence,
+                             Object[][] schedule,
+                             int initialCapacity) {
+        int lower = 0;
+        int upper = 0;
+        int size = 0;
+        int capacity = initialCapacity;
+
+        // Check empty
+        checkSizeCapacity(size, capacity);
+        checkLowerUpper(lower, upper);
+
+        // Write and read chunks according to the schedule
+        for (Object[] pair : schedule) {
+            char access = ((Character) pair[0]).charValue();
+            int amount = ((Integer) pair[1]).intValue();
+
+            // Read, write, or free a chunk
+            switch (access) {
+            case 'r':
+                for (int offset = 0; offset < amount; offset++) {
+                    assertEquals(sequence[lower + offset], buffer.get());
+                }
+                lower += amount;
+                size -= amount;
+                break;
+            case 'w':
+                for (int offset = 0; offset < amount; offset++) {
+                    buffer.put(sequence[upper + offset]);
+                }
+                upper += amount;
+                size += amount;
+                // Increase capacity if needed
+                while (size > capacity)
+                    capacity *= 2;
+                break;
+            case 'f':
+                buffer.free(lower + amount - 1);
+                lower += amount;
+                size -= amount;
+                break;
+            }
+
+            // Check the buffer
+            checkSizeCapacity(size, capacity);
+            checkLowerUpper(lower, upper);
+            for (int position = lower; position < upper; position++) {
+                assertEquals(sequence[position], buffer.getAt(position));
+            }
+        }
+    }
+}
